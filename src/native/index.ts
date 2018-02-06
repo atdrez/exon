2674d9b066a764f01140ef796fda3b15e6398491
Adriano Rezende <adriano.rezende@petitfabrik.com>
2018-02-06 00:12:53 -0200

    Added new elements into index

--- src/native/index.ts
+++ src/native/index.ts
@@ -1,6 +1,10 @@
+import { Map } from "./Map";
+import { Json } from "./Json";
+import { EvalJson } from "./EvalJson";
+import { Merge } from "./Merge";
 import { EnvVar } from "./EnvVar";
 import { Eq, Ne, Or, And, If } from "./Language";
 
 export function components() : any {
-    return [ EnvVar, Eq, Ne, Or, And, If ];
+    return [ Map, Json, EvalJson, Merge, EnvVar, Eq, Ne, Or, And, If ];
 }
