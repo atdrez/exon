9fa02ef74c9f1d2bdb8125540f4169c519e8ec1c
Adriano Rezende <atdrez@gmail.com>
2026-05-24 16:52:30 -0300

    Fixed fn.number.is handling NaN

--- runtimes/typescript/src/fn/number/is.ts
+++ runtimes/typescript/src/fn/number/is.ts
@@ -7,6 +7,6 @@ export default class Component extends OpUnary {
     constructor() { super("number.is"); }
 
     public evaluate(obj: any, value: any, _context: Context): any {
-        return (typeof value === "number");
+        return (typeof value === "number" && !Number.isNaN(value));
     }
 }
