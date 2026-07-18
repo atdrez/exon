febf270113ca9ea8b30ddaf15cfaea7fdc6e9491
Adriano Rezende <atdrez@gmail.com>
2026-07-18 13:54:12 -0300

    Added lazy-route and http-server examples

---
+++ examples/http/javascript/server.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const http = require('http');
+const { URL } = require('url');
+
+function sendJson(res, status, body) {
+    const isObject = typeof body === 'object' && body !== null;
+
+    if (!isObject) {
+        res.writeHead(status);
+        res.end(body);
+    }
+    else {
+        res.writeHead(status, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(body, null, 4).replace(/\\\\/g, '\\'));
+    }
+}
+
+module.exports.isDeferred = function() {
+    return true;
+}
+
+module.exports.resolve = function(obj, context) {
+    const port = typeof obj.port === 'number' ? obj.port : 8080;
+    const content = obj.content;
+
+    const server = http.createServer((req, res) => {
+        if (req.method !== 'GET') {
+            sendJson(res, 405, { error: 'method not allowed' });
+            return;
+        }
+
+        const url = new URL(req.url || '/', 'http://localhost');
+        const segments = url.pathname.split('/').filter(Boolean);
+        const queryParams = {};
+        url.searchParams.forEach((v, k) => { queryParams[k] = v; });
+
+        try {
+            sendJson(res, 200, context.resolveRoute(content, segments, queryParams));
+        } catch (e) {
+            const msg = e instanceof Error ? e.message : String(e);
+            sendJson(res, msg.startsWith('not found:') ? 404 : 500, { error: msg });
+        }
+    });
+
+    server.listen(port, () => {
+        process.stderr.write('Exon HTTP server listening on http://localhost:' + port + '\n');
+    });
+
+    return null;
+}
---
+++ examples/route/data/lorem.md
@@ -0,0 +1,43 @@
+# Lorem Ipsum
+
+## Introduction
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+## Features
+
+* Lorem ipsum dolor sit amet
+* Consectetur adipiscing elit
+* Sed do eiusmod tempor incididunt
+* Ut labore et dolore magna aliqua
+
+## Usage
+
+### Basic Example
+
+```text
+Lorem ipsum dolor sit amet,
+consectetur adipiscing elit.
+```
+
+### Advanced Example
+
+1. Lorem ipsum dolor sit amet.
+2. Consectetur adipiscing elit.
+3. Sed do eiusmod tempor incididunt.
+
+## Table
+
+| Item | Description                      |
+| ---- | -------------------------------- |
+| A    | Lorem ipsum dolor sit amet       |
+| B    | Consectetur adipiscing elit      |
+| C    | Sed do eiusmod tempor incididunt |
+
+## Quote
+
+> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+## Conclusion
+
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
