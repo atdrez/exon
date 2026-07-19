ec4d83caffd16e7403a5a2ed62483a13e47378ce
Adriano Rezende <atdrez@gmail.com>
2026-07-19 11:58:41 -0300

    Early return on fn.and and fn.or

--- runtimes/typescript/src/fn/lang/and.ts
+++ runtimes/typescript/src/fn/lang/and.ts
@@ -6,11 +6,11 @@ import { Context } from "../../IScript";
 export default class Component extends OpVariadic {
     constructor() { super("and", 2); }
 
-    public evaluate(obj: any, values: Array<any>, _context: Context): any {
+    public evaluate(_obj: any, values: Array<any>, _context: Context): any {
         let result = values[0];
 
-        for (let i = 1; i < values.length; i++) {
-            result = result && values[i];
+        for (let i = 1; i < values.length && result; i++) {
+            result = values[i];
         }
 
         return result;
