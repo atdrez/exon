f932169dd43302b7ac405a883658bef0d2b6d723
Adriano Rezende <atdrez@gmail.com>
2026-07-25 14:57:47 -0300

    Updated process.exec to concatenate stdout/stderr to error message

--- runtimes/typescript/src/fn/process/exec.ts
+++ runtimes/typescript/src/fn/process/exec.ts
@@ -7,8 +7,28 @@ import { OpVariadic } from "../opVariadic";
 export default class Component extends OpVariadic {
     constructor() { super("process.exec", 1); }
 
-    public evaluate(_obj: any, values: Array<any>, _context: Context) : any {
+    public evaluate(_obj: any, values: Array<any>, context: Context) : any {
         const cmd = values.join("");
-        return execSync(cmd).toString();
+
+        try {
+            return execSync(cmd).toString();
+        } catch (error: any) {
+            const stdout = error.stdout ? error.stdout.toString() : "";
+            const stderr = error.stderr ? error.stderr.toString() : "";
+
+            let message = error.message;
+
+            if (stdout.length > 0) {
+                message += "\n  [STDOUT]: ";
+                message += stdout.split("\n").join("\n    ");
+            }
+
+            if (stderr.length > 0) {
+                message += "\n  [STDERR]: ";
+                message += stderr.split("\n").join("\n    ");
+            }
+
+            context.rethrow(new Error(message), context.location);
+        }
     }
 }
