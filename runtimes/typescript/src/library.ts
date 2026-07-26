1d814bd898d7345ac38823e1ec9fd1b3b0f64329
Adriano Rezende <atdrez@gmail.com>
2026-07-26 10:42:37 -0300

    Updated tsconfig and exposed library

---
+++ runtimes/typescript/src/library.ts
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+export { Parser } from "./Parser";
+export { Resolver } from "./Resolver";
+export { IScriptRepository } from "./IScriptRepository";
+export { ScriptRepository } from "./ScriptRepository";
+export { RuntimeOptions } from "./RuntimeOptions";
+export { CallSite, LocatedError, ResolverError } from "./ResolverError";
+export * as Native from "./fn";
